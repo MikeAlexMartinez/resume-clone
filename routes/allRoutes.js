a5bb9dd07f8bc5afda65ee53d55ac0dc5bb1dd3f
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2018-01-23 17:00:29 +0000

    removed appData from routes as empty

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Application data
-const appData = require('../scripts/appData');
-
 const routes = [
   {
     method: 'get',
     route: '/',
     fn: function homePage(req, res) {
-      res.render('my-projects/resume-clone/home', {title: 'Resume', data: appData.data });
+      res.render('my-projects/resume-clone/home', {title: 'Resume'});
     }
   },
 ];
